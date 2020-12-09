e061264b0b354cf2fccc99e6d4e3b6c13f603c5c
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 14:19:38 -0600

    footer component

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    <div className="footer mt-5">
+      <a href="https://www.instagram.com/ashlihanson/?hl=en" target="_blank" className="fa fa-instagram"></a>
+      <a href="https://www.linkedin.com/in/ashli-hanson-2689091b0/" target="_blank" className="fa fa-linkedin"></a>
+      <a href="https://github.com/ashlinhanson" target="_blank" className="fa fa-github"></a>
+    </div>
+}
+
+export default Footer;
