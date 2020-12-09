2efa88ea44e063716d51ba4cd0689e80fd45be9c
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 16:43:03 -0600

    added props to project card...?

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Image } from 'cloudinary-react';
+import "../assets/projects.json";
 
-function ProjectCard() {
+function ProjectCard(props) {
     <div className="col-md-4">
             <div className="card">
-              <img src="" className="card-img-top" alt="" style="height: 350px"/>
+              <img src={props.projects.imageURL} className="card-img-top" alt="" style="height: 350px"/>
               <div className="card-body">
-                <h5 className="card-title"></h5>
+                <h5 className="card-title">{props.projects.name}</h5>
                 <p className="card-text">
+                    {props.projects.summary}
                 </p>
-                <a href="" target="_blank" className="btn btn-primary">Check it out</a>
-                <a href="" target="_blank" className="btn btn-primary">View the code</a>
+                <a href={props.projects.deployLink} target="_blank" className="btn btn-primary">Check it out</a>
+                <a href={props.projects.gitHubLink} target="_blank" className="btn btn-primary">View the code</a>
               </div>
             </div>
     </div>
