b99018f070d13bc011ef2e62e66f4ff578cbdf54
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 14:31:24 -0600

    project card container component

---
+++ portfolio/src/components/ProjectCardContainer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectCardContainer() {
+
+    <div className="card my-5" id="backgroundCard">
+        <div className="container-folio">
+            <div className="col-md-12">
+                <div className="row">
+
+                    <ProjectCard/>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default ProjectCardContainer;
