0322f14d9d30d34195142b6c51ae096707bc2eff
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 16:59:32 -0600

    fixed project card so that it maps over the json object

--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -2,20 +2,27 @@ import React from "react";
 import { Image } from 'cloudinary-react';
 import "../assets/projects.json";
 
-function ProjectCard(props) {
-    <div className="col-md-4">
-            <div className="card">
-              <img src={props.projects.imageURL} className="card-img-top" alt="" style="height: 350px"/>
-              <div className="card-body">
-                <h5 className="card-title">{props.projects.name}</h5>
-                <p className="card-text">
-                    {props.projects.summary}
-                </p>
-                <a href={props.projects.deployLink} target="_blank" className="btn btn-primary">Check it out</a>
-                <a href={props.projects.gitHubLink} target="_blank" className="btn btn-primary">View the code</a>
-              </div>
+function ProjectCard() {
+
+    return(
+    <div>
+        {projects.map(project => (
+            <div className="col-md-4">
+                <div className="card">
+                    <img src={project.imageURL} className="card-img-top" alt="" style="height: 350px"/>
+                        <div className="card-body">
+                            <h5 className="card-title">{project.name}</h5>
+                            <p className="card-text">
+                                {project.summary}
+                            </p>
+                            <a href={project.deployLink} target="_blank" className="btn btn-primary">Check it out</a>
+                            <a href={project.gitHubLink} target="_blank" className="btn btn-primary">View the code</a>
+                        </div>
+                </div>
             </div>
+        ))};
     </div>
+    );
 }
 
 export default ProjectCard;
