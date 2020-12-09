c4dafc65fdc2cd5a0a50c123512130b3d83a27bd
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 15:07:31 -0600

    refactoring portfolio container, setting state and functions

---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+
+class PortfolioContainer extends Component {
+
+   state = {
+       currentPage : "About"
+   };
+
+   handlePageChange = page => {
+       this.setState({ currentPage: page });
+   };
+
+   renderPage = () => {
+       if (this.state.currentPage === "About") {
+           return <About />;
+       } else if (this.state.currentPage === "Portfolio") {
+           return <Portfolio />;
+       } else if (this.state.currentPage === "Contact") {
+           return <Contact />;
+       }
+   };
+
+   render
+
+}
+
+export default PortfolioContainer;
