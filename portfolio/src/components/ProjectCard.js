cebe17723dbd62c6d7c8a5a2f57df7de5d5c5927
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 14:35:33 -0600

    project card component

---
+++ portfolio/src/components/ProjectCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ProjectCard() {
+    <div className="col-md-4">
+            <div className="card">
+              <img src="" className="card-img-top" alt="" style="height: 350px"/>
+              <div className="card-body">
+                <h5 className="card-title"></h5>
+                <p className="card-text">
+                </p>
+                <a href="" target="_blank" className="btn btn-primary">Check it out</a>
+                <a href="" target="_blank" className="btn btn-primary">View the code</a>
+              </div>
+            </div>
+    </div>
+}
+
+export default ProjectCard;
