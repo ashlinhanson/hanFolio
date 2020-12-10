123c87aa867c2ddc3a6007fc7205f901f1c8874e
ashlinhanson <ashlinhanson@gmail.com>
2020-12-09 21:30:20 -0600

    refactoring with Router/started on App

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
 
-const App = () => <PortfolioContainer />;
+function App() {
+   
+    <Router>
+        <div>
+            <Header />
+                <Route exact path="/" component={ About } />
+                <Route exact path="/about" component={ About } />
+                <Route exact path="/portfolio" component={ Portfolio } />
+                <Route exact path="/contact" component={ Contact } />
+            <Footer />
+        </div>
+    </Router>
+    
+
+} 
 
 export default App;
